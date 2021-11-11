c549a8e81627567aa9765c5e51b139903c39a1f3
Ian Nesbitt <ian.nesbitt@gmail.com>
2021-11-11 16:27:21 -0500

    adding module

---
+++ pyrmittivity/constants.py
@@ -0,0 +1,5 @@
+# define constants
+
+C_0 = 299792458.                # speed of light in a vacuum
+Eps_0 = 8.8541878 * 10**(-12)   # epsilon naught (vacuum permittivity)
+Mu_0 = 1.257 * 10**(-6)         # mu naught (vacuum permeability)
